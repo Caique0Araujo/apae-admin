d0090d75d3bba41bd4ec552cc0d474f7d15362fe
Matheus Ferreira <matheus.44300@gmail.com>
2022-11-28 21:23:25 -0300

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { useCookies } from "react-cookie";
 import Home from "./presenters/ui/home/home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SideMenuBarComponent from "./presenters/components/side-menu-bar-component/side-menu-bar-component";
 
 function App() {
 
@@ -13,7 +15,15 @@ function App() {
   }
 
   return (
-    <Home/>
+    <BrowserRouter>
+      <header>
+          <SideMenuBarComponent/>
+      </header>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/presenters/ui/home/home.jsx
+++ src/presenters/ui/home/home.jsx
@@ -1,14 +1,9 @@
 import { Container } from 'react-bootstrap';
-import SideMenuBarComponent from '../../components/side-menu-bar-component/side-menu-bar-component';
 import './css/Home.min.css';
 
 export default function Home() {
     return (
         <div>
-            <header>
-                <SideMenuBarComponent/>
-            </header>
-
             <main>
                 <Container className='pt-4'>
                     <h2>Hello World</h2>
