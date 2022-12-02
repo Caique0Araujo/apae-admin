41da5010ec76995ccabd4ba90442ef5cb4596d65
Matheus Ferreira <matheus.44300@gmail.com>
2022-12-01 22:49:24 -0300

    Updated Users Repository

--- src/infra/repositories/user-repository.js
+++ src/infra/repositories/user-repository.js
@@ -1,33 +1,29 @@
 import { API_URL } from '../../settings.js';
 
 export async function postLogin(login, password) {
-    try {
-        const data = {
-            'login': login,
-            'password': password
-        };
+    const data = {
+        'login': login,
+        'password': password
+    };
 
-        const requestData = {
-            body: JSON.stringify(data),
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
+    const requestData = {
+        body: JSON.stringify(data),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
         }
-
-        const response = await fetch(`${API_URL}/users/login`, requestData);
-        const json = await response.json();
-        if (response.status !== 200) {
-            let error = {
-                msg: json
-            };
-            throw error;
-        }
-
-        return json;
-    } catch (ex) {
-        throw ex;
     }
+
+    const response = await fetch(`${API_URL}/users/login`, requestData);
+    const json = await response.json();
+    if (response.status !== 200) {
+        let error = {
+            msg: json
+        };
+        throw error;
+    }
+
+    return json;
 }
 
 export async function postCreateUser(name, login, password, token) {
