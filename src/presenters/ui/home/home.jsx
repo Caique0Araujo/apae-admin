2fb80af54ac7b881c95fb743ec7bd2052e59f5d9
Matheus Ferreira <matheus.44300@gmail.com>
2022-12-07 22:20:16 -0300

    Started style of home page

--- src/presenters/ui/home/home.jsx
+++ src/presenters/ui/home/home.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { GlobalContext } from '../../utils/context';
+import { FaUserAlt } from 'react-icons/fa';
 
 export default function Home() {
 
@@ -11,12 +12,23 @@ export default function Home() {
     }, [setActive]);
 
     return (
-        <div>
-            <main>
-                <Container className='pt-4'>
-                    <h2>Hello World</h2>
-                </Container>
-            </main>
-        </div>
+        <main className='pt-4'>
+            <Container className='p-5'>
+                <Row className='pt-5'>
+                    <Col>
+                        <div className='d-flex p-5 rounded' style={{backgroundColor: '#F2F2F2'}}>
+                            <div className='p-5 rounded-circle' style={{backgroundColor: '#666666'}}>
+                                <FaUserAlt size={120}/>
+                            </div>
+                            Usuário Atual
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='py-5'>
+                    <Col className='pe-4'>Noticias</Col>
+                    <Col className='ps-4'>Produtos</Col>
+                </Row>
+            </Container>
+        </main>
     );
 }
