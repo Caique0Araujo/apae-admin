e56b3a936442d78524b92997699ddac5c0cefcce
Matheus Ferreira <matheus.44300@gmail.com>
2023-02-19 13:01:18 -0300

    Added text overflow in title of news

--- src/presenters/ui/news/components/news-item/news-item.jsx
+++ src/presenters/ui/news/components/news-item/news-item.jsx
@@ -12,7 +12,7 @@ export default function NewsItem(props) {
 
     return (
         <Col key={props.id} className={`news-item d-flex flex-column justify-content-between px-3 py-2 rounded border border-2 text-center m-2 ${props.active && 'active'}`} onClick={() => props.onClick(props.id)}>
-            <span className="title">{title}</span>
+            <span className="title">{props.title}</span>
             <div className="image-container rounded mt-2 bg-white">
                 <img src={`data:image/png;base64,${buffer}`} alt={props.title} height={150} />
             </div>
